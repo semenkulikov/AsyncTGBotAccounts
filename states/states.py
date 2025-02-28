28acff77420d58e1a6d97339ca06fb0aab4553f7
Semen Saifutdinov <saifutdinov2006@mail.ru>
2025-02-28 15:58:53 +0300

    Updated states

--- states/states.py
+++ states/states.py
@@ -3,3 +3,9 @@ from aiogram.fsm.state import StatesGroup, State
 
 class AdminPanel(StatesGroup):
     get_users = State()
+
+
+class AddAccountStates(StatesGroup):
+    wait_phone = State()
+    wait_code = State()
+    wait_2fa = State()
